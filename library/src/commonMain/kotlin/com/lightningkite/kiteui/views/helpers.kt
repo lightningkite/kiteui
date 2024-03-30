06267e140280f41e5a4aa769780a83e460664cef
Joseph Ivie <joseph@lightningkite.com>
2024-03-29 18:18:09 -0600

    New modifiers for gravity

--- library/src/commonMain/kotlin/com/lightningkite/kiteui/views/helpers.kt
+++ library/src/commonMain/kotlin/com/lightningkite/kiteui/views/helpers.kt
@@ -6,6 +6,17 @@ import com.lightningkite.kiteui.reactive.invoke
 import com.lightningkite.kiteui.views.direct.*
 
 @ViewModifierDsl3 val ViewWriter.centered get() = gravity(Align.Center, Align.Center)
+@ViewModifierDsl3 val ViewWriter.atStart get() = gravity(Align.Start, Align.Stretch)
+@ViewModifierDsl3 val ViewWriter.atEnd get() = gravity(Align.End, Align.Stretch)
+@ViewModifierDsl3 val ViewWriter.atTop get() = gravity(Align.Stretch, Align.Start)
+@ViewModifierDsl3 val ViewWriter.atBottom get() = gravity(Align.Stretch, Align.End)
+@ViewModifierDsl3 val ViewWriter.atTopStart get() = gravity(Align.Start, Align.Start)
+@ViewModifierDsl3 val ViewWriter.atBottomStart get() = gravity(Align.Start, Align.End)
+@ViewModifierDsl3 val ViewWriter.atTopCenter get() = gravity(Align.Center, Align.Start)
+@ViewModifierDsl3 val ViewWriter.atBottomCenter get() = gravity(Align.Center, Align.End)
+@ViewModifierDsl3 val ViewWriter.atTopEnd get() = gravity(Align.End, Align.Start)
+@ViewModifierDsl3 val ViewWriter.atBottomEnd get() = gravity(Align.End, Align.End)
+
 @ViewModifierDsl3 val ViewWriter.expanding get() = weight(1f)
 @ViewModifierDsl3 operator fun ViewWrapper.minus(unit: Unit) = Unit
 @ViewModifierDsl3 operator fun ViewWrapper.minus(wrapper: ViewWrapper) = ViewWrapper
