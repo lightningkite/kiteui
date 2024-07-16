bd9f36aba6f2be59911d7163e6cabc88ead42df0
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2024-07-15 21:33:22 -0400

    Background sound loop proof-of-concept

--- example-app/src/commonMain/kotlin/com/lightningkite/mppexampleapp/AudioScreen.kt
+++ example-app/src/commonMain/kotlin/com/lightningkite/mppexampleapp/AudioScreen.kt
@@ -1,18 +1,17 @@
 package com.lightningkite.mppexampleapp
 
 import com.lightningkite.kiteui.*
-import com.lightningkite.kiteui.models.AudioLocal
-import com.lightningkite.kiteui.models.AudioRemote
 import com.lightningkite.kiteui.models.AudioSource
 import com.lightningkite.kiteui.navigation.Screen
-import com.lightningkite.kiteui.reactive.Property
-import com.lightningkite.kiteui.reactive.await
-import com.lightningkite.kiteui.reactive.awaitNotNull
+import com.lightningkite.kiteui.reactive.*
 import com.lightningkite.kiteui.views.*
 import com.lightningkite.kiteui.views.direct.*
 
 @Routable("audio")
 object AudioScreen : Screen {
+
+    val backgroundSoundPlaying = PersistentProperty("backgroundNoisePlaying", false)
+
     override fun ViewWriter.render() {
         val soundEffectPool = SoundEffectPool()
         col {
@@ -27,6 +26,43 @@ object AudioScreen : Screen {
 //            withPool("CantinaBand3.wav", "https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav")
 //            withPool("StarWars3.wav", "https://www2.cs.uic.edu/~i101/SoundFiles/StarWars3.wav")
             withPool("Taunt", Resources.audioTaunt)
+
+            toggleButton {
+                checked bind backgroundSoundPlaying
+                text("Background sound")
+            }
+
+            val backgroundAudioShared = shared {
+                Resources.audioTaunt.load().apply { volume = 0.1f }
+            }
+            // Loop and cancel the background sound
+            reactiveScope {
+                if (backgroundSoundPlaying()) {
+                    val backgroundAudio = backgroundAudioShared()
+                    suspendCoroutineCancellable<Unit> {
+                        backgroundAudio.onComplete {
+                            backgroundAudio.play()
+                        }
+                        return@suspendCoroutineCancellable {
+                            backgroundAudio.stop()
+                        }
+                    }
+                }
+            }
+            // Trigger the background sound once every five seconds until it successfully starts
+            reactiveScope {
+                if (backgroundSoundPlaying()) {
+                    val backgroundAudio = backgroundAudioShared()
+                    while (true) {
+                        if (!backgroundAudio.isPlaying) {
+                            backgroundAudio.play()
+                            delay(5000)
+                        } else {
+                            break
+                        }
+                    }
+                }
+            }
         }
     }
 
