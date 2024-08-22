ebc52ad54dc846fe43ad5b7f9078580aec071d78
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2024-08-21 19:37:03 -0600

    Color fix for iOS ProgressBar

--- library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/ProgressBar.ios.kt
+++ library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/ProgressBar.ios.kt
@@ -7,15 +7,14 @@ import com.lightningkite.kiteui.views.*
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.CoreGraphics.CGRectMake
 import platform.UIKit.UIProgressView
-import platform.UIKit.UIView
 
 @OptIn(ExperimentalForeignApi::class)
 actual class ProgressBar actual constructor(context: RContext) : RView(context) {
     override val native = UIProgressView(CGRectMake(0.0, 0.0, 0.0, 0.0))
     override fun applyForeground(theme: Theme) {
         super.applyForeground(theme)
-        native.trackTintColor = theme.foreground.closestColor().toUiColor()
-        native.progressTintColor = theme.card().background.closestColor().toUiColor()
+        native.trackTintColor = theme.background.closestColor().toUiColor()
+        native.progressTintColor = theme.card().foreground.closestColor().toUiColor()
     }
     actual var ratio: Float
         get() = native.progress
