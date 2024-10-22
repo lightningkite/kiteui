acf7ccbbbfa1bdd91fed0b5fe48a59545dbcedc2
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2024-10-22 16:01:16 -0600

    Post-merge fixes

--- library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/Video.ios.kt
+++ library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/Video.ios.kt
@@ -7,6 +7,7 @@ import com.lightningkite.kiteui.printStackTrace2
 import com.lightningkite.kiteui.reactive.*
 import com.lightningkite.kiteui.views.*
 import kotlinx.cinterop.*
+import kotlinx.coroutines.launch
 import platform.AVFoundation.*
 import platform.AVKit.AVPlayerViewController
 import platform.AVKit.AVPlayerViewControllerDelegateProtocol
