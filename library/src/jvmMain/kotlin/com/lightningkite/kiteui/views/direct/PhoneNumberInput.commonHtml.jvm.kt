928eb09bcb0203277182b2bd091f7345058dbd24
Joseph Ivie <joseph@lightningkite.com>
2024-11-08 14:41:03 -0700

    Removed extraneous file that broke building the JVM

--- library/src/jvmMain/kotlin/com/lightningkite/kiteui/views/direct/PhoneNumberInput.commonHtml.jvm.kt
+++
@@ -1,5 +0,0 @@
-package com.lightningkite.kiteui.views.direct
-
-actual val PhoneNumberInput.selectionStart: Int? get() = null
-actual val PhoneNumberInput.selectionEnd: Int? get() = null
-actual fun PhoneNumberInput.setSelectionRange(start: Int, end: Int) {}
