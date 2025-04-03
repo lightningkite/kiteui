746e612cae00aa6dae17344995732eae21ef07b7
Trax Bagley <kf7mxe@gmail.com>
2025-04-03 10:27:06 -0600

    Better method of passing in the android setAutoFillHints

--- library/src/androidMain/kotlin/com/lightningkite/kiteui/views/direct/TextField.android.kt
+++ library/src/androidMain/kotlin/com/lightningkite/kiteui/views/direct/TextField.android.kt
@@ -217,11 +217,13 @@ var EditText.keyboardHints: KeyboardHints
         n.inputType = inputType
         if(VERSION.SDK_INT >= VERSION_CODES.O) {
             when (value.autocomplete) {
-                AutoComplete.Email -> View.AUTOFILL_HINT_EMAIL_ADDRESS
-                AutoComplete.Password, AutoComplete.NewPassword -> View.AUTOFILL_HINT_PASSWORD
-                AutoComplete.Phone -> View.AUTOFILL_HINT_PHONE
-                AutoComplete.OneTimeCode -> HintConstants.AUTOFILL_HINT_SMS_OTP + ","+ HintConstants.AUTOFILL_HINT_EMAIL_OTP
+                AutoComplete.Email -> arrayOf(View.AUTOFILL_HINT_EMAIL_ADDRESS)
+                AutoComplete.Password, AutoComplete.NewPassword -> arrayOf(View.AUTOFILL_HINT_PASSWORD)
+                AutoComplete.Phone -> arrayOf(View.AUTOFILL_HINT_PHONE)
+                AutoComplete.OneTimeCode -> arrayOf(HintConstants.AUTOFILL_HINT_SMS_OTP, HintConstants.AUTOFILL_HINT_EMAIL_OTP)
                 null -> null
-            }?.let { n.setAutofillHints(it) }
+            }?.let {
+                n.setAutofillHints(*it)
+            }
         }
     }
