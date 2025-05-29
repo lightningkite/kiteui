5121104dba209b34225b348678c093d36612f8c6
Joseph Ivie <joseph@lightningkite.com>
2025-05-29 11:49:05 -0600

    Hash updates in unit test

--- library/src/jsTest/kotlin/Recycler2Test.kt
+++ library/src/jsTest/kotlin/Recycler2Test.kt
@@ -6,8 +6,8 @@ import kotlin.test.Test
 class Recycler2Test {
     @Test fun scrollRules() = assertManualReview(
         file = "ScrollView.commonHtml.js.kt",
-        currentHash = "fba7a2cd837572a70abc5359b1fbdbd244240cfd",
-        reviewedHash = "fba7a2cd837572a70abc5359b1fbdbd244240cfd",
+        currentHash = "b30a46c0314a45938d3820107dc23dbfac21ecd8",
+        reviewedHash = "b30a46c0314a45938d3820107dc23dbfac21ecd8",
         whatToTest = """
             This file is *extremely sensitive* to view pagers and recycler views.
             As such, upon editing this file you must retest manually:
@@ -21,8 +21,8 @@ class Recycler2Test {
     )
     @Test fun r2Bullshit() = assertManualReview(
         file = "Recycler2.kt",
-        currentHash = "f11c93d365428bb99d3841015aa5a592c2d80a59",
-        reviewedHash = "f11c93d365428bb99d3841015aa5a592c2d80a59",
+        currentHash = "fc8956b20063588b6e6b7e0b1186788f71f7508c",
+        reviewedHash = "fc8956b20063588b6e6b7e0b1186788f71f7508c",
         whatToTest = """
             This file is *extremely sensitive* to view pagers and recycler views.
             As such, upon editing this file you must retest manually:
