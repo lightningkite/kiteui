f7c0adb8240098bf261d9c268ed69a2a5c42ad05
Joseph Ivie <joseph@lightningkite.com>
2025-06-11 10:08:37 -0600

    Backport blob to byte array TextView iOS

--- library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/TextView.ios.kt
+++ library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/TextView.ios.kt
@@ -122,7 +122,7 @@ actual class TextView actual constructor(context: RContext) : RView(context) {
     actual var lineClamp: Int? = null
         set(value) {
             field = value
-            label.numberOfLines = value ?: 0
+            label.numberOfLines = value?.toLong() ?: 0L
             if (value != null && label.lineBreakMode != NSLineBreakByTruncatingTail) {
                 label.lineBreakMode = NSLineBreakByTruncatingTail
             }
