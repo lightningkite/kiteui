5ba9fb6caf77d0d047171bafba28164da154b899
iHoonter <huntdawghero@gmail.com>
2025-07-02 17:09:18 -0600

    Added a way to guarantee a negative input option to KeyboardHints

--- library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/NumberField.ios.kt
+++ library/src/iosMain/kotlin/com/lightningkite/kiteui/views/direct/NumberField.ios.kt
@@ -71,7 +71,7 @@ actual class NumberInput actual constructor(context: RContext) : RViewWithAction
                     dirty = textField.text ?: "",
                     selectionStart = textField.selectedTextRange?.start?.let { textField.offsetFromPosition(textField.beginningOfDocument, it) }?.toInt(),
                     selectionEnd = textField.selectedTextRange?.end?.let { textField.offsetFromPosition(textField.beginningOfDocument, it) }?.toInt(),
-                    allowDecimal = keyboardHints != KeyboardHints.integer,
+                    allowDecimal = keyboardHints.allowDecimal,
                     setResult = {
                         textField.text = it
                     },
@@ -136,9 +136,9 @@ actual class NumberInput actual constructor(context: RContext) : RViewWithAction
             }
             textField.keyboardType = when (value.type) {
                 KeyboardType.Text -> UIKeyboardTypeDefault
-                KeyboardType.Integer -> UIKeyboardTypeNumberPad
+                KeyboardType.Integer, KeyboardType.IntegerWithNegative -> UIKeyboardTypeNumberPad
                 KeyboardType.Phone -> UIKeyboardTypePhonePad
-                KeyboardType.Decimal -> UIKeyboardTypeNumbersAndPunctuation
+                KeyboardType.Decimal, KeyboardType.DecimalWithNegative -> UIKeyboardTypeNumbersAndPunctuation
                 KeyboardType.Email -> UIKeyboardTypeEmailAddress
             }
             textField.textContentType = when (value.autocomplete) {
