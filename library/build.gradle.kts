ebc6ee59162ae71d19f0c9d5ffd43b2f4a89d4d1
UnknownJoe796 <josephivie@gmail.com>
2026-03-25 13:50:30 -0600

    Use CDN for driver screenshots in JS

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -138,9 +138,6 @@ kotlin {
         }
         val jsMain by getting {
             dependsOn(commonHtmlMain)
-            dependencies {
-                implementation(npm("modern-screenshot", "4.4.39"))
-            }
         }
     }
 
--- library/src/jsMain/kotlin/com/lightningkite/kiteui/views/DriverScreenshot.js.kt
+++ library/src/jsMain/kotlin/com/lightningkite/kiteui/views/DriverScreenshot.js.kt
@@ -5,7 +5,7 @@ import kotlin.js.Promise
 
 @OptIn(kotlin.io.encoding.ExperimentalEncodingApi::class)
 actual suspend fun RView.driverScreenshot(): String {
-    val mod = js("import('modern-screenshot')").unsafeCast<Promise<dynamic>>().await()
+    val mod = js("import('https://esm.sh/modern-screenshot@4.4.39')").unsafeCast<Promise<dynamic>>().await()
     val element = this.native.element ?: throw DriverActionException("Element not yet attached to DOM")
     val dataUrl = (mod.domToPng(element) as Promise<String>).await()
     val prefix = "data:image/png;base64,"
