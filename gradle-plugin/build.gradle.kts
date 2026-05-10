0215b0260f2e9f45d6dfef8d4f06275b4244b629
Trax Bagley <kf7mxe@gmail.com>
2026-05-09 23:57:10 -0600

    fix gradle issue with plugin

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,107 +1,39 @@
-// by Claude
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import com.lightningkite.deployhelpers.*
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.kotlinCocoapods)
-    alias(libs.plugins.kotlinPluginSerialization)
-    alias(libs.plugins.androidLibrary)
+    `java-gradle-plugin`
+    `kotlin-dsl`
     signing
     alias(libs.plugins.vannitechPublishing)
     alias(libs.plugins.dokka)
 }
-val isMac = System.getProperty("os.name").startsWith("Mac OS")
 
-kotlin {
-    applyDefaultHierarchyTemplate()
-
-    androidTarget {
-        publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
+gradlePlugin {
+    plugins {
+        create("lightningkite-kiteui") {
+            id = "com.lightningkite.kiteui"
+            implementationClass = "com.lightningkite.kiteui.KiteUiPlugin"
         }
-    }
-    iosX64()
-    if (isMac) {
-        iosArm64()
-        iosSimulatorArm64()
-    }
-    js(IR) {
-        browser()
-    }
-    jvm("jvmSsr")
-
-    compilerOptions {
-        freeCompilerArgs.add("-Xexpect-actual-classes")
-        optIn.add("kotlinx.cinterop.BetaInteropApi")
-        optIn.add("kotlinx.cinterop.ExperimentalForeignApi")
-        optIn.add("kotlin.time.ExperimentalTime")
-        optIn.add("kotlin.uuid.ExperimentalUuidApi")
-    }
-    cocoapods {
-        name = "GradlePlugin"
-        summary = "KiteUI Camera and Barcode Scanning Support"
-        homepage = "https://github.com/lightningkite/kiteui"
-        version = "1.0"
-        if (isMac) {
-            ios.deploymentTarget = "14.0"
-        }
-    }
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(project(":library"))
-            }
-        }
-
-        val androidMain by getting {
-            dependencies {
-                // CameraX
-                api("androidx.camera:camera-core:1.4.2")
-                api("androidx.camera:camera-camera2:1.4.2")
-                api("androidx.camera:camera-lifecycle:1.4.2")
-                api("androidx.camera:camera-view:1.4.2")
-                // ML Kit Barcode Scanning
-                api("com.google.mlkit:barcode-scanning:17.3.0")
-            }
-        }
-        val isMac = System.getProperty("os.name").startsWith("Mac OS")
-        if (isMac) {
-            val iosMain by getting
-        }
-
-        val jsMain by getting {
-            dependencies {
-                // Spec-compliant BarcodeDetector polyfill using ZXing WASM
-                implementation(npm("barcode-detector", "3.0.8"))
-            }
-        }
-
-        val jvmSsrMain by getting
     }
 }
 
-android {
-    namespace = "GradlePlugin"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 21
-    }
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    dependencies {
-        coreLibraryDesugaring(libs.desugar.jdk.libs)
-    }
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation(kotlin("gradle-plugin-api"))
+    implementation(libs.fontbox)
+    testImplementation(libs.junit)
+}
+tasks.validatePlugins {
+    enableStricterValidation.set(true)
 }
 
-lkLibrary("lightningkite", "kiteui-camera") {
-    description.set("KiteUI Camera and Barcode Scanning Support")
+tasks.create("publishLocally", Copy::class.java) {
+    from(file("src/main/kotlin/KiteUiPlugin.kt"))
+    into(rootProject.file("buildSrc/src/main/kotlin"))
+}
+
+lkLibrary("lightningkite", "kiteui") {
+    description.set("Automatically create your routers")
 }
