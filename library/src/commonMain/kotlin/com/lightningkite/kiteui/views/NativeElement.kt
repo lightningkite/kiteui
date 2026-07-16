b2c6a30d02c4ff187a8c9054f64c3efb8faa4f9b
iHoonter <huntdawghero@gmail.com>
2026-07-16 11:24:07 -0600

    Silenced warning unless in debug mode

--- library/src/commonMain/kotlin/com/lightningkite/kiteui/views/NativeElement.kt
+++ library/src/commonMain/kotlin/com/lightningkite/kiteui/views/NativeElement.kt
@@ -514,7 +514,7 @@ abstract class NativeElementCommonCode internal constructor(override val context
 
     /** The final computed padding combining theme padding, custom padding, and safe area insets */
     val appliedPadding: Edges get() {
-        if (!fullyStarted) Log.warn("$this attempted to calculate applied padding before fully started.")
+        if (!fullyStarted && debugMode) Log.warn("$this attempted to calculate applied padding before fully started.")
         return (paddingByEdge ?: theme.padding.takeIf { themeAndBack.padding } ?: Edges.ZERO).let { p ->
             safeAreaPadding?.let { p + it } ?: p
         }
