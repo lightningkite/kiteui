39bee856dfe48857400156570b79cefeb084311a
Trax Bagley <kf7mxe@gmail.com>
2026-07-23 12:52:48 -0600

    Android 17 require Explicitly grant read and write permissions to the receiving camera app

--- library/src/androidMain/kotlin/com/lightningkite/kiteui/ExternalServices.android.kt
+++ library/src/androidMain/kotlin/com/lightningkite/kiteui/ExternalServices.android.kt
@@ -248,6 +248,9 @@ private suspend fun requestImageCamera(
         if (!it.accepted) return@requestPermissions cont.resume(null)
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, file)
+
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+
         if (front) {
             intent.putExtra("android.intent.extras.LENS_FACING_FRONT", 1)
             intent.putExtra("android.intent.extras.CAMERA_FACING", 1)
